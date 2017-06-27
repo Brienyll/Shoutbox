b85fbf76839726421795fac4ef37d2e7a1eb3f6e
Brix <Brix.angeles@gmail.com>
2017-06-27 14:46:58 -0700

    added validation and inserted shouts

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'database.php' ; ?>
 <?php
   //Create Query
-  $query = "SELECT * FROM shouts";
+  $query = "SELECT * FROM shouts ORDER BY id DESC";
   $shouts = mysqli_query($con, $query);
  ?>
 <!DOCTYPE html>
@@ -24,10 +24,13 @@
         </ul>
       </div>
       <div id="input">
+        <?php if(isset($_GET['error'])) : ?>
+          <div class="error"><?php echo $_GET['error']; ?></div>
+        <?php endif; ?>
         <form method="post" action="process.php">
           <input type="text" name="user" placeholder="Enter Your Name" />
-          <input type="text" name="message" placeholder="Enter Your Message" />
-          <br/>
+          <input type="text" name="message" placeholder="Enter A Message" />
+          <br />
           <input class="shout-btn" type="submit" name="submit" value="Shout it out" />
         </form>
       </div>
